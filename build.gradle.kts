6162b8942dbf51f5bcc6d23bafd619a591fca813
devle <dltjgus0709@gmail.com>
2023-11-17 23:43:01 +0900

    init: Add Gradle build files

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.9.20"
+}
+
+group = "com.devlee79"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(11)
+}
